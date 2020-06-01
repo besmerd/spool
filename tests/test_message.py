960cd7f3aed92ead750e78f5a1cd45989f04d0b2
Daniel Besmer <besmerd@gmail.com>
2020-06-01 10:15:34 +0200

    Add test case for message module

---
+++ tests/test_message.py
@@ -0,0 +1,53 @@
+import pytest
+
+from email.parser import HeaderParser
+from mailman.message import Message
+
+combinations = [
+    (
+        {
+            "sender": "sender@example.org",
+            "recipients": "recipient@example.org"
+        },
+        "sender@example.org",
+        "recipient@example.org",
+    ),
+    (
+        {
+            "sender": "sender@example.com",
+            "from_addr": "sender@example.org",
+            "recipients": "recipient@example.org",
+        },
+        "sender@example.org",
+        "recipient@example.org",
+    ),
+    (
+        {
+            "sender": "sender@example.org",
+            "to_addrs": "recipient@example.org",
+            "recipients": "recpient@example.com",
+        },
+        "sender@example.org",
+        "recipient@example.org",
+    ),
+    (
+        {
+            "sender": "sender@example.com",
+            "from_addr": "sender@example.org",
+            "recipients": "recipient@example.com",
+            "to_addrs": "recipient@example.org",
+        },
+        "sender@example.org",
+        "recipient@example.org",
+    ),
+]
+
+
+@pytest.mark.parametrize("config, from_header, to_header", combinations)
+def test_rfc822_headers(config, from_header, to_header):
+    msg = Message(**config).as_string()
+    parsed = HeaderParser().parsestr(msg)
+
+    assert "from" in parsed and parsed.get("from") == from_header
+    assert "to" in parsed and parsed.get("to") == to_header
+    assert "subject" in parsed
